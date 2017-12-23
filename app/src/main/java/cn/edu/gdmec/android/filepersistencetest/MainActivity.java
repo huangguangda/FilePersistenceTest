10ff57e44c26253327a3ece5993bdb3a88249bf0
huangguangda <2397923107@qq.com>
2017-12-24 00:37:44 +0800

    1

--- app/src/main/java/cn/edu/gdmec/android/filepersistencetest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/filepersistencetest/MainActivity.java
@@ -1,8 +1,14 @@
 package cn.edu.gdmec.android.filepersistencetest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edit;
@@ -22,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void save(String inputText){
-
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try{
+            out = openFileOutput ( "data", Context.MODE_PRIVATE );
+            writer = new BufferedWriter ( new OutputStreamWriter ( out ) );
+            writer.write ( inputText );
+        }catch (IOException e){
+            e.printStackTrace ();
+        }finally {
+            try{
+                if (writer != null){
+                    writer.close ();
+                }
+            }catch (IOException e){
+                e.printStackTrace ();
+            }
+        }
     }
 }
