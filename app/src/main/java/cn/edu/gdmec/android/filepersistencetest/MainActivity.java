eda344030a8fe8df4bd8e3b394e77c432d7e79f6
huangguangda <2397923107@qq.com>
2017-12-24 00:01:04 +0800

    1

--- app/src/main/java/cn/edu/gdmec/android/filepersistencetest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/filepersistencetest/MainActivity.java
@@ -2,12 +2,26 @@ package cn.edu.gdmec.android.filepersistencetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+        edit = (EditText) findViewById ( R.id.edit );
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy ();
+        String inputText = edit.getText ().toString ();
+        save(inputText);
+    }
+
+    public void save(String inputText){
+
     }
 }
